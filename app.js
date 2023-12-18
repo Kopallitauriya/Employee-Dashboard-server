34cd2aeb9c59d23f05d7c44665e196de8c328baf
Kopal Litauriya <110325444+Kopallitauriya@users.noreply.github.com>
2023-12-19 01:42:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mockdata = require("./data")
 
 const app = express()
 
-app.use(cors({origin:["https://employee-dashboard-client-kopal-litauriyas-projects.vercel.app", "http://localhost:3000"]}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
